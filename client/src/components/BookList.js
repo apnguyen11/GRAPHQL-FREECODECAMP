6b2897d2061e7a465185485891b61dcfb6efe0dd
Andy Nguyen <apnguyen11@yahoo.com>
2020-02-11 14:30:17 -0600

    created new query to retrieve a single book to be displayed when book is clicked on list

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { graphql} from 'react-apollo'
 import {getBooksQuery} from '../queries/queries'
 
+//components
+import BookDetails from './BookDetails'
+
  class BookList extends Component {
      displayBooks(){
          var data = this.props.data;
@@ -21,6 +24,7 @@ import {getBooksQuery} from '../queries/queries'
                 <ul id="book-list">
                     {this.displayBooks()}
                 </ul>
+                <BookDetails />
             </div>
         )
     }
