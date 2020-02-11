108859727804f2e210d247ec3109ce9c158fb281
Andy Nguyen <apnguyen11@yahoo.com>
2020-02-11 13:04:47 -0600

    created booklist components that renders books from db to list on client. created addbook component that renders authors from db to dropdown menu on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import BookList from './components/BookList';
 import  { default as ApolloClient } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+
+//components
+import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+
 
 // apollo client setup
 const client = new ApolloClient({
@@ -14,6 +18,7 @@ function App() {
     <div id="main">
       <h1>Andy's Reading List</h1>
       <BookList />
+      <AddBook />
     </div>
     </ApolloProvider>
   );
